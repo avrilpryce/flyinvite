27b56f871658ea22369546dee31bdd0377b83330
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-24 17:34:34 +0100

    Index and Show methods, routes, and views done for #trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,9 @@
 class TripsController < ApplicationController
   def index
-    @trips = Trip.where(trip.host = current_user)
+    @trips = current_user.owned_trips
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
   end
 end

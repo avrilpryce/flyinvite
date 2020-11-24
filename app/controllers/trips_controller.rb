b0cab6de9a099035898b325a41b111d0d755bbdf
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-24 18:16:24 +0100

    New and Create done for #trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,4 +6,26 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @host = current_user
+    @trip.host = @host
+
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :street, :zip, :city, :country, :start_date, :end_date, :airport_code, :max_price, :flight_class, :latest_arrival, :earliest_departure)
+  end
 end

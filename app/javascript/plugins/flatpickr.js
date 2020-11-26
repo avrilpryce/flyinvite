00325163062aa9b0af09c14949bd1c07a0e41be3
Mareike Heuer <mareike.heuer@hotmail.com>
2020-11-26 17:23:46 +0100

    Flatpickr on create trip form complete

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,5 +4,16 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 flatpickr("#range_start", {
   altInput: true,
-  plugins: [new rangePlugin({ input: "#range_end" })]
+  plugins: [new rangePlugin({ input: "#range_end" })],
+
+});
+
+flatpickr("#latest_arrival", {
+  altInput: true,
+  enableTime: true
+});
+
+flatpickr("#earliest_departure", {
+  altInput: true,
+  enableTime: true
 });

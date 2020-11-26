703260155f80c509a476f00ad9989d154e06beac
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-26 16:17:12 +0100

    grid and flexb on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in? && current_user.host
+      @owned_trips = current_user.owned_trips
+    end
+    if user_signed_in?
+      @trips = current_user.trips
+      @all_trips = Trip.all
+    end
   end
 end
